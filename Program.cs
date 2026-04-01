a85d6eb8b909a1c629dc1c7a45954d44abeb6f78
hckerson <hckerson@gmail.com>
2026-04-01 17:54:10 +0100

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System.Globalization;
+
+namespace MyApp.Services;
+
+class Greeter
+{
+    public string Greet(string name)
+    {
+        var culture = CultureInfo.CurrentCulture;
+        return $"Hello, {name}! Culture: {culture.Name}";
+    }
+}
